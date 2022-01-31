609d67c4a625d43f131c8509c1cd4d2a7763d164
Leandra-pdf <lmnop@localhost>
2022-01-31 17:39:02 -0500

    Second assignment

--- large-scale-programming-assignments/src/org/howard/edu/lsp/assignment2/Token.java
+++ large-scale-programming-assignments/src/org/howard/edu/lsp/assignment2/Token.java
@@ -1,10 +1,41 @@
 package org.howard.edu.lsp.assignment2;
 
+import java.util.*;
+import java.util.stream.*; 
+/**
+ * @author leandramcphie
+ */
 public class Token {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+    /**
+	 * @param args
+	 */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);  //user input scanner
+        System.out.println("String?");
+        String token = input.nextLine();         
+        
+        while (token != "Goodbye") {					//condition to break loop
+        	
+            String newStr = token.replace(" ", "\n");               //gets rid of spaces
+            List<String> Tlist = Arrays.asList(token.split(" "));               
+            
+            List<Integer> TlistInt = Tlist.stream().map(Integer::parseInt)  //changes string into int
+             .collect(Collectors.toList());
+            
+    
+    		int sum = 0;                                    //calculates and display sum and products of tokens
+    		int prod = 1;
+    		for (int i = 0; i < TlistInt.size(); i++) {
+    			sum += TlistInt.get(i);
+    			prod *= TlistInt.get(i);
+    		}
+    		
+    		System.out.println("Tokens:\n" + newStr);
+    		System.out.println("Sum: "+sum);
+    		System.out.println("Product: "+prod+"\n");      
+    		System.out.println("String?");
+    		
+    		token = input.nextLine();					//takes next user input
+        }                    
 	}
-
 }
