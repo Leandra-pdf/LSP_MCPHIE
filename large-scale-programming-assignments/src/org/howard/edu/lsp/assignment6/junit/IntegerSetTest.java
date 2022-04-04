d95a0cd948bcbf0df0bd96f88e67c828e9ac39d6
Leandra-pdf <lmnop@10.4.23.40>
2022-04-03 23:25:44 -0400

    Saving my progress for assignment 6

---
+++ large-scale-programming-assignments/src/org/howard/edu/lsp/assignment6/junit/IntegerSetTest.java
@@ -0,0 +1,199 @@
+package org.howard.edu.lsp.assignment6.junit;
+
+import org.howard.edu.lsp.assignment6.integerset.IntegerSet;
+import org.howard.edu.lsp.assignment6.integerset.IntegerSetException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author Leandra McPhie
+ */
+
+public class IntegerSetTest {
+	
+	private static final boolean True = false;
+	private static final boolean False = false;
+
+	@Test
+	@DisplayName("Test case for clear method")
+	public void testClear() {
+		//JUnit test cases for the clear method
+		IntegerSet set1 = new IntegerSet();
+		assertTrue(set1.isEmpty());
+		
+		set1.add(1);
+		set1.add(2);
+		assertFalse(set1.isEmpty());
+		
+		set1.clear();
+		assertTrue(set1.isEmpty());
+		
+		set1.add(3);
+		assertFalse(set1.isEmpty());
+		
+		set1.clear();
+		assertTrue(set1.isEmpty());
+	}
+	
+	
+	@Test
+	@DisplayName("Test case for equals length method")
+	public void testLength() {
+		//JUnit test cases for the length method
+		IntegerSet set1 = new IntegerSet();
+		assertTrue(set1.length() == 0);
+		
+		set1.add(1);
+		set1.add(2);
+		set1.add(3);
+		set1.add(4);
+		set1.add(5);
+		assertEquals(set1.length(), 5);
+		assertFalse(set1.length() == 0);
+		assertTrue(set1.length() > 1);
+		
+		set1.clear();
+		set1.add(6);
+		assertTrue(set1.length() == 1);
+		assertFalse(set1.length() < 1);
+	}
+	
+	
+	@Test
+	@DisplayName("Test case for equals method")
+	public void testEquals() {
+		//JUnit test cases for the equals method
+		IntegerSet set1 = new IntegerSet();
+		IntegerSet set2 = new IntegerSet();
+		IntegerSet set3 = new IntegerSet();
+		assertTrue(set2.equals(set1) == True);
+		assertTrue(set3.equals(set2) == True);
+		
+		set1.add(1);
+		set1.add(2);
+		assertTrue(set2.equals(set1) == False);
+		assertFalse(set2.equals(set1));
+		
+		set2.add(1);
+		set2.add(1);
+		set3.add(1);
+		set3.add(1);
+		assertFalse(set3.equals(set1));
+		assertTrue(set3.equals(set1) == False);
+		assertTrue(set3.equals(set2));
+		
+		set3.clear();
+		set3.add(1);
+		set3.add(2);
+		assertFalse(set3.equals(set1) == False);
+		assertTrue(set1.equals(set3));		
+	}
+
+	
+	@Test
+	@DisplayName("Test case for contains method")
+	public void testContains() {
+		//JUnit test cases for the contains method
+		IntegerSet set1 = new IntegerSet();
+		assertTrue(set1.contains(7) == False);
+		
+		set1.add(7);
+		assertTrue(set1.contains(7));
+		assertTrue(set1.contains(7) != False);
+		assertFalse(set1.contains(6));
+		
+		set1.clear();
+		set1.add(8);
+		set1.add(9);
+		assertFalse(set1.contains(7));
+		assertTrue(set1.contains(8));
+		assertTrue(set1.contains(9));
+	}
+	
+	
+	@Test
+	@DisplayName("Test case for largest method")
+	public void testLargest() throws Exception{
+		//JUnit test cases for the largest method
+		IntegerSet set1 = new IntegerSet();
+		
+		set1.add(5);
+		assertTrue(set1.largest() > 0);
+		
+		set1.add(4);
+		set1.add(2);
+		assertTrue(set1.largest() != 4);
+		assertFalse(set1.largest() == 2);
+		assertTrue(set1.largest() == 5);
+		assertEquals(5, set1.largest());
+		assertNotEquals(2, set1.largest());
+	}
+	
+	
+	@Test
+	@DisplayName("Test case for largest method, throws exception")
+	public void testLargest_throwsException(){
+		//JUnit test cases for the largest method
+		IntegerSet set1 = new IntegerSet();
+		Exception exception1 = assertThrows(IntegerSetException.class, () -> {
+			set1.largest(); 
+		});
+		
+		String expectedMessage = "Exception from largest because set is empty.";
+		String actualMessage = exception1.getMessage();
+		assertTrue(actualMessage.contains(expectedMessage) == True);
+	}
+	
+	
+	@Test
+	@DisplayName("Test case for smallest method")
+	public void testSmallest() throws Exception{
+		//JUnit test cases for the largest method
+		IntegerSet set1 = new IntegerSet();
+		
+		set1.add(5);
+		set1.add(4);
+		set1.add(2);
+		assertTrue(set1.smallest() != 4);
+		assertTrue(set1.smallest() == 2);
+		assertFalse(set1.smallest() == 5);
+		assertEquals(2, set1.smallest());
+		assertNotEquals(4, set1.smallest());
+	}
+	
+	
+	@Test
+	@DisplayName("Test case for smallest method, throws exception")
+	public void testSmallest_throwsException(){
+		//JUnit test cases for the largest method
+		IntegerSet set1 = new IntegerSet();
+		Exception exception1 = assertThrows(IntegerSetException.class, () -> {
+			set1.largest(); 
+		});
+		
+		String expectedMessage = "Exception from largest because set is empty.";
+		String actualMessage = exception1.getMessage();
+		assertTrue(actualMessage.contains(expectedMessage) == True);
+	}
+	
+	
+	@Test
+	@DisplayName("IntegerSet.add() test cases")
+	public void testAdd() {
+		//JUnit test cases for the add method
+		IntegerSet set1 = new IntegerSet();
+		assertTrue(set1.toString().equals(""));
+		
+		set1.add(7);
+		set1.add(3);
+		set1.add(6);
+		assertNotEquals(set1.toString(), "7 3 6");
+		assertEquals(set1.toString(), "7 3 6 ");
+	}
+	
+	
+}
